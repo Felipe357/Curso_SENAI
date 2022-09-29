6bdd84e66894302e6e813ffa7b6db6c86879aa8c
Felipe357 <felipeserra.100@gmail.com>
2022-09-29 10:55:57 -0300

    Components

---
+++ 2-DES/pwbe/Aula09/docs/bd/scrpit.sql
@@ -0,0 +1,22 @@
+
+drop database if exists Patrimonio;
+create database Patrimonio charset=UTF8 collate utf8_general_ci;
+use Patrimonio;
+
+create table itens(
+    ni numeric(6) not null,
+    aquisicao timestamp not null,
+    denominacao varchar(100) not null,
+    valor numeric(10,2) not null,
+    img varchar(200) default 'default.png',
+    constraint pk_item primary key (ni)
+);
+
+LOAD DATA INFILE 'C:/Users/DESENVOLVIMENTO/Desktop/Curso_SENAI/2-DES/pwbe/Aula09/docs/bd/itens.csv'
+INTO TABLE itens
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 ROWS;
+
+select * from itens;-
