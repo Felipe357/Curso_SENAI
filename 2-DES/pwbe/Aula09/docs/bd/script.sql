0051d9e6ebf9b2ad6c13abe4990aaf7252ca6c14
Felipe357 <felipeserra.100@gmail.com>
2022-10-05 08:15:04 -0300

    Att

--- 2-DES/pwbe/Aula09/docs/bd/script.sql
+++ 2-DES/pwbe/Aula09/docs/bd/script.sql
@@ -1,17 +1,18 @@
-
 drop database if exists Patrimonio;
 create database Patrimonio charset=UTF8 collate utf8_general_ci;
 use Patrimonio;
-
+-- DDL
 create table itens(
     ni numeric(6) not null,
     aquisicao timestamp not null,
     denominacao varchar(100) not null,
-    valor numeric(10,2) not null,
+    valor numeric(10,2),
     img varchar(200) default 'default.png',
     constraint pk_item primary key (ni)
 );
 
+describe itens;
+-- DML
 LOAD DATA INFILE 'C:/Users/DESENVOLVIMENTO/Desktop/Curso_SENAI/2-DES/pwbe/Aula09/docs/bd/itens.csv'
 INTO TABLE itens
 FIELDS TERMINATED BY ','
@@ -19,4 +20,4 @@ ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
 IGNORE 1 ROWS;
 
-select * from itens;-
+select * from itens;
