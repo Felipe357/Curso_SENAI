d3e725eff5d7f52ac4a58fbc9f268a96371a671e
Felipe357 <felipeserra.100@gmail.com>
2022-11-08 11:20:04 -0300

    Snippets

---
+++ 2-DES/snippets/script.js
@@ -0,0 +1,11 @@
+const  = (req, res) => {
+    con.query(.(req.params), (err, result) => {
+        if (err == null)
+            if (result.affectedRows > 0)
+                res.status(204).end();
+            else
+                res.status(404).end();
+        else
+            res.status(400).json(err).end();
+    });
+}
