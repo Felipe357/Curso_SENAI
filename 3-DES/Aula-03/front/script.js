07ff1ab499f3ae74e7d064a993b4599add4d6fd4
Felipe357 <felipeserra.100@gmail.com>
2023-02-06 15:45:31 -0300

    Att

--- 3-DES/Aula-03/front/script.js
+++ 3-DES/Aula-03/front/script.js
@@ -1,5 +1,20 @@
-var btnS = document.querySelector(".btnSetor")
+var produto = document.querySelector(".modal-produto")
+var setor = document.querySelector(".modal-setor")
+var funcionario = document.querySelector(".modal-funcionario")
+function btnSetor() {
+    setor.style.top = "300px"
+    funcionario.style.bottom = "-400px"
+    produto.style.left = "-400px"
+}
 
-const btnSetor = () => {
+function btnProduto() {
+    setor.style.top = "-400px"
+    funcionario.style.bottom = "-400px"
+    produto.style.left = "180px"
+}
 
+function btnFuncionario() {
+    setor.style.top = "-400px"
+    funcionario.style.bottom = "300px"
+    produto.style.left = "-400px"
 }
